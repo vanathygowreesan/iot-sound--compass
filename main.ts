578d3e3dcb34c84fbf66335955771595dc23ff2d
Chris Bull <christopher.bull@dcmail.ca>
2022-03-14 13:46:41 -0400

    Changed melodies to notes

--- main.ts
+++ main.ts
@@ -3,27 +3,27 @@ basic.forever(function () {
     direction = input.compassHeading()
     if (direction <= 22.5 || direction > 337.5) {
         basic.showString("N")
-        music.playMelody("C5 B A G F E D C ", 120)
+        music.playTone(165, music.beat(BeatFraction.Whole))
     } else if (direction > 22.5 && direction <= 67.5) {
         basic.showString("NW")
-        music.playMelody("C D E F G A B C5 ", 120)
+        music.playTone(220, music.beat(BeatFraction.Whole))
     } else if (direction > 67.5 && direction <= 112.5) {
         basic.showString("W")
-        music.playMelody("E B C5 A B G A F ", 120)
+        music.playTone(294, music.beat(BeatFraction.Whole))
     } else if (direction > 112.5 && direction <= 157.5) {
         basic.showString("SW")
-        music.playMelody("A F E F D G E F ", 120)
+        music.playTone(349, music.beat(BeatFraction.Whole))
     } else if (direction > 157.5 && direction <= 202.5) {
         basic.showString("S")
-        music.playMelody("C5 A B G A F G E ", 175)
+        music.playTone(440, music.beat(BeatFraction.Whole))
     } else if (direction > 202.5 && direction <= 247.5) {
         basic.showString("SE")
-        music.playMelody("G B A G C5 B A B ", 175)
+        music.playTone(523, music.beat(BeatFraction.Whole))
     } else if (direction > 247.5 && direction <= 292.5) {
         basic.showString("E")
-        music.playMelody("E E G A E F E C5 ", 175)
+        music.playTone(587, music.beat(BeatFraction.Whole))
     } else if (direction > 292.5 && direction <= 337.5) {
         basic.showString("NE")
-        music.playMelody("G F G A - F E D ", 175)
+        music.playTone(784, music.beat(BeatFraction.Whole))
     }
 })
